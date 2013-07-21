9aab8834f8e3f9e583bd0ea316021d459d280401
OldYin <alexsinclair@softhome.net>
2013-07-21 04:05:35 +0100

    GUI graphics path made case sensitive when Optifine installed. Used lower case MOD_ID String to ensure lower case path.
    Issue caused by Optifine but may crop up in some other situations.
    
    Might have been easier to simply change MOD_ID to all lowercase in CoreLib but this is where I went with it.

--- WR-common/wirelessredstone/core/lib/GuiLib.java
+++ WR-common/wirelessredstone/core/lib/GuiLib.java
@@ -3,15 +3,15 @@ package wirelessredstone.core.lib;
 import net.minecraft.util.ResourceLocation;
 
 public class GuiLib {
-
+	private static final String L_MOD_ID=CoreLib.MOD_ID.toLowerCase(); //Cludge to ensure lowercase path.
 	private static final String GUI_PREFIX = "textures/gui/";
-	public static final ResourceLocation GUI_ON =		new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "guiOn.png");
-	public static final ResourceLocation GUI_OFF =		new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "guiOff.png");
-	public static final ResourceLocation GUI_EXIT = 	new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "wifi_exit.png");
-	public static final ResourceLocation GUI_SMALL = 	new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "wifi_small.png");
-	public static final ResourceLocation GUI_MEDIUM =	new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "wifi_medium.png");
-	public static final ResourceLocation GUI_LARGE =	new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "wifi_large.png");
-	public static final ResourceLocation GUI_XLARGE =	new ResourceLocation(CoreLib.MOD_ID, GUI_PREFIX + "wifi_xlarge.png");
+	public static final ResourceLocation GUI_ON =		new ResourceLocation(L_MOD_ID, GUI_PREFIX + "guiOn.png");
+	public static final ResourceLocation GUI_OFF =		new ResourceLocation(L_MOD_ID, GUI_PREFIX + "guiOff.png");
+	public static final ResourceLocation GUI_EXIT = 	new ResourceLocation(L_MOD_ID, GUI_PREFIX + "wifi_exit.png");
+	public static final ResourceLocation GUI_SMALL = 	new ResourceLocation(L_MOD_ID, GUI_PREFIX + "wifi_small.png");
+	public static final ResourceLocation GUI_MEDIUM =	new ResourceLocation(L_MOD_ID, GUI_PREFIX + "wifi_medium.png");
+	public static final ResourceLocation GUI_LARGE =	new ResourceLocation(L_MOD_ID, GUI_PREFIX + "wifi_large.png");
+	public static final ResourceLocation GUI_XLARGE =	new ResourceLocation(L_MOD_ID, GUI_PREFIX + "wifi_xlarge.png");
 	public static final int GUIID_INVENTORY = 0;
 	public static final int GUIID_DEVICE = 1;
 }
